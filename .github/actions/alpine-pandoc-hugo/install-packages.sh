03df0037b613dddb652d9c095d33bfa1bb7a41b3
Carsten Gips <cagix@fh-bielefeld.de>
2021-08-30 09:12:01 +0200

    docker: use latest hugo version (manual install)

--- .github/actions/alpine-pandoc-hugo/install-packages.sh
+++ .github/actions/alpine-pandoc-hugo/install-packages.sh
@@ -6,5 +6,8 @@ tlmgr install beamertheme-metropolis pgfopts tcolorbox environ || exit 1
 ## Install extra packages for LaTeX conversion to .png
 tlmgr install standalone filemod currfile || exit 1
 
-## Install Hugo and make and graphviz (dot)
-apk --no-cache add hugo make git bash graphviz ghostscript || exit 1
+## Install make and graphviz (dot)
+apk --no-cache add make bash graphviz ghostscript || exit 1
+
+## Install current Hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.87.0/hugo_0.87.0_Linux-64bit.tar.gz && tar -zxf hugo_*.tar.gz -C /usr/bin/ && rm hugo_*.tar.gz  || exit 1
